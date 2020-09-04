9930adb9c769bca02cf8bdaa9c14c09bae30cf70
ZHELTOV Roman <boumrz@gmail.com>
2020-09-04 10:59:41 +0300

    fix cross

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,4 +1,3 @@
-export const CROSS = '<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' width="100%" height="100%" fill="#fff">\n'
-    + '<defs />\n'
-    + '<path d=\'M10.586 12L4 18.586V20h1.414L12 13.414 18.586 20H20v-1.414L13.414 12 20 5.414V4h-1.414L12 10.586 5.414 4H4v1.414z\' />\n'
-    + '        </svg>';
+export const CROSS = '<svg width="28" height="28" viewBox="0 0 28 28" fill="white" xmlns="http://www.w3.org/2000/svg">\n'
+    + '<path d="M11.5255 14L0 25.5255V28H2.4745L14 16.4745L25.5255 28H28V25.5255L16.4745 14L28 2.4745V0H25.5255L14 11.5255L2.4745 0H0V2.4745L11.5255 14Z"/>\n'
+    + '</svg>\n';
